2d22ed257cd44e00696eeb1e5eb1ab60a360f832
nemaher <nicholasemaher@gmail.com>
2017-07-22 18:29:52 -0500

    finishing spinner

--- app/src/main/java/len/fidgetr/ColorWheel.java
+++ app/src/main/java/len/fidgetr/ColorWheel.java
@@ -2,35 +2,46 @@ package len.fidgetr;
 
 import android.graphics.Color;
 
-import java.util.Random;
-
 /**
  * Created by NicholasMaher on 7/10/17.
  */
 
 public class ColorWheel {
-    private String[] mColors ={
-            "#39add1", // light blue
-            "#3079ab", // dark blue
-            "#c25975", // mauve
-            "#e15258", // red
-            "#f9845b", // orange
-            "#838cc7", // lavender
-            "#7d669e", // purple
-            "#53bbb4", // aqua
-            "#51b46d", // green
-            "#e0ab18", // mustard
-            "#637a91", // dark gray
-            "#f092b0", // pink
-            "#b7c0c7"  // light gray
+
+    private String[] mColors={
+            "#ff4000",
+            "#ff8000",
+            "#ffbf00",
+            "#ffff00",
+            "#bfff00",
+            "#80ff00",
+            "#40ff00",
+            "#00ff00",
+            "#00ff40",
+            "#00ff80",
+            "#00ffbf",
+            "#00ffff",
+            "#00bfff",
+            "#0080ff",
+            "#0040ff",
+            "#0000ff",
+            "#4000ff",
+            "#8000ff",
+            "#bf00ff",
+            "#ff00ff",
+            "#ff00bf",
+            "#ff0080",
+            "#ff0040",
+            "#ff0000"
     };
 
-    public int getColor(){
+    public int getColor(int nextColor){
         String color;
 
-        Random randomGenerator = new Random();
-        int randomNumber = randomGenerator.nextInt(mColors.length);
-        color = mColors[randomNumber];
+        //Random randomGenerator = new Random();
+        //int randomNumber = randomGenerator.nextInt(mColors.length);
+
+        color = mColors[nextColor];
         int colorAsInt = Color.parseColor(color);
 
         return colorAsInt;
